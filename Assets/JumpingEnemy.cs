9cf477eeb104a4bfd9f77ad3c5fe8a25a51530ed
vratislavino <vratislavino@gmail.com>
2022-01-10 16:08:25 +0100

    heh

---
+++ Assets/JumpingEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingEnemy : MonoBehaviour
+{
+    public float jumpForce = 700;
+    public float jumpCooldown = 2f;
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (true) { }
+        rigid = GetComponent<Rigidbody2D>();
+        StartCoroutine(Jump());
+        //InvokeRepeating("Jump", 0, jumpCooldown);
+    }
+
+    public IEnumerator Jump() {
+        while (true) {
+            rigid.AddForce(Vector2.up * jumpForce);
+            yield return new WaitForSeconds(jumpCooldown);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,21 +9,55 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpForce = 100;
 
+    bool isGrounded = true;
+    public List<Transform> GroundingPoints;
+    public float maxSpeed = 6;
+
     // Start is called before the first frame update
     void Start()
     {   
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    private void UpdateIsGrounded() {
+
+        /*Debug.DrawLine(
+            GroundingPoint.position, 
+            GroundingPoint.position + (Vector3.down*0.3f), 
+            Color.red, 
+            1f);*/
+
+        isGrounded = GroundingPoints.Any(point => {
+            return Physics2D.Raycast(point.position, Vector2.down, 0.3f).collider != null; ;
+        });
+    }
+
+    void ClampHorizontalVelocity() {
+        var vel = rigid.velocity;
+        if(vel.x < -maxSpeed) {
+            vel.x = -maxSpeed;
+        } else if (vel.x > maxSpeed) {
+            vel.x = maxSpeed;
+        }
+        rigid.velocity = vel;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        UpdateIsGrounded();
+
+
         var input = Input.GetAxis("Horizontal");
         rigid.AddForce(Vector2.right * input * speed);
 
-        var jump = Input.GetButtonDown("Jump");
-        if(jump) {
-            rigid.AddForce(Vector2.up * jumpForce);
+        if (isGrounded) {
+            var jump = Input.GetButtonDown("Jump");
+            if (jump) {
+                rigid.AddForce(Vector2.up * jumpForce);
+            }
         }
+
+        ClampHorizontalVelocity();
     }
 }
